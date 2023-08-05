bcb2745962e3d6dceb48db49bfe6f2692e5a141d
LuizAGDomingues <luizarthur1234@gmail.com>
2023-08-05 17:28:41 -0300

    upd: change metadata

--- .history/src/app/layout_20230805172736.tsx
+++ .history/src/app/layout_20230805172736.tsx
@@ -1,6 +1,6 @@
+import './globals.css'
 import type { Metadata } from 'next'
 import { Jacques_Francois } from 'next/font/google'
-import './globals.css'
 
 const jacques = Jacques_Francois({weight: '400', subsets: ['latin']})
 
